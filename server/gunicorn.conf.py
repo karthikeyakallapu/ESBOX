2ca54583d0068c7bbeea004e5ce6a009f06a6430
Karthikeya Kallapu <karthikeyakallapu@gmail.com>
2026-02-06 18:01:33 +0530

    updated gunicorn conf

--- server/gunicorn.conf.py
+++ server/gunicorn.conf.py
@@ -1,6 +1,8 @@
 import multiprocessing
+import os
 
-bind = "0.0.0.0:8000"
+bind = f"0.0.0.0:{os.environ.get('PORT', '8000')}"
+
 workers = multiprocessing.cpu_count() * 2 + 1
 worker_class = "uvicorn.workers.UvicornWorker"
 
